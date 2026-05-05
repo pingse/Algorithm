c482523d314f972f6a90cfdd92a64bfddaef9ce8
YongJu Kim (Pin) <54977093+pingse@users.noreply.github.com>
2026-05-05 23:15:45 +0900

    [level 0] Title: 커피 심부름, Time: 0.28 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,9 @@
+def solution(order):
+    answer = 0
+    
+    for i in range(len(order)):
+        if ('americano' in order[i] or 'anything' == order[i]):
+            answer += 4500
+        elif ('cafelatte' in order[i]):
+            answer += 5000
+    return answer
